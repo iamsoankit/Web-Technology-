5bb6a297b166e7a52f7668899c0f6b1ac07d42be
iamsoankit <98892759+iamsoankit@users.noreply.github.com>
2023-02-22 12:55:09 +0530

    Week 5

---
+++ Java_Lab5/QuickSort.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("number of elements: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        Random rd = new Random();
+        for (int i = 0; i < n; i++) {
+            a[i] = rd.nextInt(1000);
+        }
+        System.out.println("unsorted array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        System.out.println("sorted array is: ");
+        QuickSort(a, 0, n - 1);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void swap(int[] a, int i, int j) {
+        int temp = 0;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static int partition(int[] a, int low, int high) {
+        int i = low + 1;
+        int j = high;
+        int pivot = a[low];
+        while (i <= j) {
+            while (i <= high && a[i] <= pivot) {
+                i++;
+            }
+            while (j > low && a[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                swap(a, i, j);
+            }
+        }
+        swap(a, low, j);
+        return j;
+    }
+
+    public static void QuickSort(int[] a, int low, int high) {
+        if (low < high) {
+            int pivot = partition(a, low, high);
+            QuickSort(a, low, pivot - 1);
+            QuickSort(a, pivot + 1, high);
+        }
+
+    }
+}
